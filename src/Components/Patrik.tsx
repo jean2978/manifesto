563156cd5ea9c8be1796329b68c445af1e31f45a
Kpop <chupatrik@gmail.com>
2021-03-18 20:38:53 +0100

    hiya

--- src/Components/Patrik.tsx
+++ src/Components/Patrik.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, MotionConfig } from "framer-motion";
+import { motion } from "framer-motion";
 
 
 export const Patrik: React.FC = () => {
@@ -8,7 +8,7 @@ export const Patrik: React.FC = () => {
         <motion.div 
               whileTap={{ scale: 0.3 }} 
               style={{backgroundColor: 'black', color: 'white', textAlign: 'center', }}
-        >Bajs</motion.div>
+        >Bajskörv</motion.div>
         </>
     )
 }
