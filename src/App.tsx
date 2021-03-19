ec4b12f252f7a7bbe3947a8b1d64ad9bf2753408
jean2978 <jeppe1934@hotmail.com>
2021-03-19 18:21:48 +0100

    whatever

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Form } from './Components/Form';
-import { Jesper } from './Components/Jesper';
-import { Patrik } from './Components/Patrik';
+import React from "react";
+import { Form } from "./Components/Form";
+import { Jesper } from "./Components/jesper";
+import { Patrik } from "./Components/Patrik";
 
 function App() {
   return (
-    <div className="App" style={{width: "100%" , height: "100%"}}>
+    <div className="App" style={{ width: "100%", height: "100%" }}>
       <Jesper />
       <Patrik />
       <Form />
--- src/Components/jesper.tsx
+++ src/Components/jesper.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
 
 export const Jesper: React.FC = () => {
-
-    return (
-        <div style={{ position: "fixed", top: "50%", left: "50%" }}>
-            <motion.img
-                src="https://cdn.discordapp.com/attachments/782707330974220348/822177787019919360/IMG_0189.PNG"
-                animate={{ rotate: 180, cx: [200, 200] }}
-                transition={{ repeat: Infinity, duration: 1 }}
-                height={"200px"}
-                width={"200px"}
-            />
-        </div>
-    )
-}
-
-
+  return (
+    <div style={{ position: "fixed", top: "50%", left: "50%" }}>
+      <motion.img
+        src="https://cdn.discordapp.com/attachments/782707330974220348/822177787019919360/IMG_0189.PNG"
+        animate={{ rotate: 360, cx: [200, 200] }}
+        transition={{ repeat: Infinity, duration: 0 }}
+        height={"200px"}
+        width={"200px"}
+      />
+    </div>
+  );
+};
